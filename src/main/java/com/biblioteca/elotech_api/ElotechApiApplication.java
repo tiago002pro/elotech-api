b0dd4c3edbd15a812f8da8e3269e24138830a7dd
tiago002pro <tiagobarbosa02@outlook.com>
2024-08-09 19:29:55 -0300

    Criação do projeto

---
+++ src/main/java/com/biblioteca/elotech_api/ElotechApiApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.elotech_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElotechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElotechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biblioteca/elotech_api/ElotechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.elotech_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElotechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
